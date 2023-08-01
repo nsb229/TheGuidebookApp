03455d2acbe92273e28318a29628a88aaef01f2b
Naina <ns1388@scarletmail.rutgers.edu>
2023-08-01 13:33:29 -0400

    Initial Commit

---
+++ TheGuidebookApp/TheGuidebookAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheGuidebookAppApp.swift
+//  TheGuidebookApp
+//
+//  Created by Nayana Sharma on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct TheGuidebookAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
